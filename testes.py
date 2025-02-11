27be9a1e5ea46b0ebce427eb1328b9835b18574b
Theo Lourencao <theo.lourencao@ifood.com.br>
2025-02-11 20:54:15 -0300

    adicionei o novo end point de produtos

--- testes.py
+++ testes.py
@@ -13,3 +13,11 @@ def test_ola_mundo():
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"mensagem": "Olá Mundo!"}
+
+def test_listar_produtos_status_code():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+def test_tamanho_lista_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
